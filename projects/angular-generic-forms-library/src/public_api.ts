cd1c5bf6bd8bfa5d65192c42e7ffba18f194c1bb
EU\HuibertM <marco.huiberts@unisys.com>
2018-05-09 11:03:07 +0200

    remove projects

--- projects/angular-generic-forms-library/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-generic-forms-library
- */
-
-export * from './lib/angular-generic-forms-library.service';
-export * from './lib/angular-generic-forms-library.component';
-export * from './lib/angular-generic-forms-library.module';
